6ad3f3a1557795c82be1a2525b8f2d6977b46f6f
Greisson Silva <greisson@bootweb.com.br>
2020-02-18 12:09:51 -0300

    'Initial-Commit'

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { MdShoppingBasket } from 'react-icons/md'
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+function Header({ cart }) {
+  console.log(cart);
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes"/>
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>{cart.length} itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
+
+export default connect(state => ({
+  cart: state.cart,
+}))(Header);
