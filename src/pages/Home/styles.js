7d7175e6e622dd2e47c283a3226a8cce682fa0e5
Greisson Silva <greisson@bootweb.com.br>
2020-02-18 18:34:07 -0300

    'Ajustes-finais.'

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,6 +7,14 @@ export const ProductList = styled.ul`
   grid-gap: 20px;
   list-style: none;
 
+  @media (max-width: 62em) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width:42em) {
+    grid-template-columns: 1fr;
+  }
+
   li {
     display: flex;
     flex-direction: column;
